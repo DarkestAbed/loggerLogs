58dc8ac0be8cea5514ae7827081e71a50200329a
Javier Thompson <javier.thompson@kaufmann.cl>
2024-09-04 18:21:14 -0400

    trying to fix simple logger

--- loggerLogs/convenient.py
+++ loggerLogs/convenient.py
@@ -4,19 +4,18 @@ from logging import Logger
 
 from loggerLogs.configs import (
     LOGGING_LEVEL_LOGGER,
+    LOGGING_LEVEL_CONSOLE,
     LEVELS,
 )
 from loggerLogs.customlogger import CustomLogger
 
 
-def convenient_logger(log_level: int = LOGGING_LEVEL_LOGGER) -> CustomLogger:
-    print(f"Initializing log on level {LEVELS.get(log_level)}")
-    return CustomLogger(
-        log_level=log_level,
-        console_log=True,
-        console_log_level=log_level,
-        file_log=False,
-        file_log_level=None,
-    )
-
-logs: Logger = convenient_logger().logger
+custom_logger: CustomLogger = CustomLogger(
+    log_level=LOGGING_LEVEL_LOGGER,
+    console_log=True,
+    console_log_level=LOGGING_LEVEL_CONSOLE,
+    file_log=False,
+    file_log_level=None,
+)
+print(f"Initializing simple logger on level {LEVELS.get(LOGGING_LEVEL_LOGGER)}.")
+logs: Logger = custom_logger.logger
